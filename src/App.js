826934f4bd5dff2b6f948e52c4bcaf33253b434d
Shehryar-Irshad <shehryarirshad28@gmail.com>
2023-05-06 12:19:17 +0500

    load

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
+import { Component } from "react";
+import { SearchBox } from "./Components/SearchBox";
+import { CardList } from "./Components/CardList";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(){
+    super(); 
+
+    this.state = {
+      pokemons: [],
+      search:''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=500')
+    .then(response => response.json())
+    .then(name => this.setState({pokemons:name.results}));
+  }
+
+  handleChange=(e) => {
+    this.setState({search: e.target.value});
+  };
+
+  render(){
+
+    const {pokemons, search } = this.state;
+    const fileteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
+
+    return(
+      <div className="App">
+        <h4><a href="https://www.linkedin.com/in/surojit-manna" target="_blank" noreferrer>Author</a></h4>
+        <h1>Pokemon Database</h1>
+        <SearchBox
+          placeholder='Search Pokemon' 
+          handleChange= {this.handleChange}
+        />
+
+        <CardList pokemons={fileteredPokemons}></CardList>
+
+      </div>
+    );
+  }
 }
 
 export default App;
